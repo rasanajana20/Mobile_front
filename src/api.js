82777c07a3fea6a7e3ec11f861147db58dd01cb5
Pinimal Rasanjana Jayarathne <134790518+rasanajana20@users.noreply.github.com>
2026-05-21 13:33:40 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mobile-back-api-jffu.vercel.app/", // backend url
+  baseURL: "https://mobile-back-api-jffu.vercel.app/api", // backend url
 });
 
 // add token if available
